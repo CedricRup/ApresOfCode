93fa7c851fdf2d967c2ea76ee62a3d423202b69b
Cédric Rup <cedric.rup@gmail.com>
2022-02-18 09:21:23 +0100

    Day 3 part 2 needs some refactoring

--- fsharp/Day3.fs
+++ fsharp/Day3.fs
@@ -17,22 +17,23 @@ let sample = @"00100
 00010
 01010"
 
+let getColumnFromReport (diagnosticReport: String list) column   =
+        diagnosticReport |> List.map (fun word -> word[column])
+
+let determineMostCommonBit (values:int*int) numberOfSamples list =
+    let sum = list |> List.sumBy (string>>int)
+    if 2 * sum >= numberOfSamples then fst values else snd values  
+
 
 let calculate (values:int*int) (diagnosticReport: String list)  =
 
     let numberOfSamples = List.length diagnosticReport
 
-    let getColumnFromReport column  =
-        diagnosticReport |> List.map (fun word -> word[column])
-
-    let determineMostCommonBit list =
-        let sum = list |> List.sumBy (string>>int)
-        if sum > numberOfSamples / 2 then fst values else snd values  
-
+    
     let wordLength = diagnosticReport.Head.Length
 
     [0.. wordLength-1] 
-    |> List.map (getColumnFromReport >> determineMostCommonBit)
+    |> List.map (getColumnFromReport diagnosticReport >> (determineMostCommonBit values numberOfSamples))
     |> List.reduce (fun word bit-> (word <<< 1) + bit)
 
 
@@ -85,3 +86,57 @@ let ``Day 3 part 1`` () =
     let gamma = calculateGamma report
     let epsilon = calculateEpsilon report 
     Assert.Equal(1092896,gamma*epsilon)
+
+
+
+let calculateOxygenGeneratorRate (diagnosticReport: String list) =
+    let rec glop (diagnosticReport: String list) column =
+        let numberOfSamples = List.length diagnosticReport
+        let mostCommonBit = 
+            getColumnFromReport diagnosticReport column
+            |> determineMostCommonBit (1,0) numberOfSamples
+        let remainingWords = diagnosticReport |> List.filter (fun word -> ( string word[column] |> int) = mostCommonBit)
+        match remainingWords with
+            | [] -> failwith "Bang"
+            | [word] -> word
+            | array -> if (array.Head.Length > (column + 1)) then glop remainingWords (column+1) else array[0] 
+
+    let word = glop diagnosticReport 0
+    word.Split() |> Array.toList |> List.map int  |> List.reduce (fun word bit-> (word <<< 1) + bit)
+
+
+
+[<Fact>]
+let ``oxygen generator rating on one bit word with equality 1 wins`` () =
+    let sample = [
+        "0"
+        "1"]    
+    let result = calculateOxygenGeneratorRate sample
+    Assert.Equal(1,result)
+
+[<Fact>]
+let ``oxygen generator rating on one bit word`` () =
+    let sample = [
+        "0"
+        "1"
+        "1"]    
+    let result = calculateOxygenGeneratorRate sample
+    Assert.Equal(1,result)
+
+[<Fact>]
+let ``oxygen generator rating on one bit word where 0 wins`` () =
+    let sample = [
+        "0"
+        "1"
+        "0"]    
+    let result = calculateOxygenGeneratorRate sample
+    Assert.Equal(0,result)
+
+[<Fact>]
+let ``oxygen generator rating ????`` () =
+    let sample = [
+        "01"
+        "11"
+        "00"]    
+    let result = calculateOxygenGeneratorRate sample
+    Assert.Equal(1,result)
