5232f20b7a51667839b766ebc132a61972e013b8
Cédric Rup <cedric.rup@gmail.com>
2022-04-10 17:41:24 +0200

    Day 7 part 1

---
+++ fsharp/Common.fs
@@ -0,0 +1,13 @@
+﻿module fsharp.Common
+
+open FParsec.Primitives
+open FParsec.CharParsers
+
+type UserState = unit // doesn't have to be unit, of course
+type Parser<'t> = Parser<'t, UserState>
+let intSeparatedByColumnParser : Parser<int32 list> = sepBy pint32 (pstring ",")
+
+let resultOrDie = function
+            | Success(result, _, _) -> result
+            | Failure(s, _, _) -> failwith s
+
--- fsharp/Day7.fs
+++ fsharp/Day7.fs
@@ -1,11 +1,33 @@
 module Day7
 
-open System
 open Xunit
 open FsUnit.Xunit
-open FSharpx.String
+open FParsec
+open fsharp.Common
 
-     
+
+let sampleInput = [16;1;2;0;4;2;7;1;2;14]
+
+let align crabPositions destination =
+    List.sumBy (fun p -> abs (p - destination)) crabPositions 
+
+let minimumConsumptionForAlignment crabPositions =
+    let minimumPostion = List.min crabPositions
+    let maximumPostion = List.max crabPositions
+    [minimumPostion..maximumPostion] |> List.map (align crabPositions) |> List.min
+   
 [<Fact>]
-let ``A test``() =
-    ()
+let ``Can calculate fuel consumption for 1 crab``() =
+    align sampleInput 1 |> should equal 41
+    
+[<Fact>]
+let ``Can calculate minimal full consumption for alignement``() =
+    minimumConsumptionForAlignment sampleInput |> should equal 37
+
+[<Fact>]
+let ``Day 7 part 1`` () =
+        System.IO.File.ReadAllText "day7Input.txt"
+        |> run intSeparatedByColumnParser
+        |> resultOrDie
+        |> minimumConsumptionForAlignment
+        |> should equal 348664
