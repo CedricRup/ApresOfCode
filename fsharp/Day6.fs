e5e4d25eb6592eb266779bf4f5d6cc9823ba9b56
Cédric Rup <cedric.rup@gmail.com>
2022-04-21 13:41:39 +0200

    All Day 6

--- fsharp/Day6.fs
+++ fsharp/Day6.fs
@@ -4,21 +4,44 @@ open Xunit
 open FsUnit.Xunit
 
 let countFishAfterDays numberOfDays initialState  =
-    let initialSchool = List.countBy id initialState |> Map.ofList
-    let addToSchool toAdd alreadyThere = 
+    let BigIntCountBy list = List. countBy id list |> List.map (fun (key,value) -> (key, int64 value))
+    let initialSchool = BigIntCountBy initialState |> Map.ofList
+    let addToSchool toAdd alreadyThere =
         match alreadyThere with
         | None -> Some toAdd
         | Some number -> Some (toAdd+number)
-    let bringSchoolToNextGen stateSchool key value =
+    let bringSchoolToNextGen newSchools key value =
         match key with
-        | 0 -> Map.change 6 (addToSchool value) stateSchool |> Map.add 8 value
-        | 7 -> Map.change 6 (addToSchool value) stateSchool
-        | e -> Map.add (e-1) value stateSchool
-    let countFishesInAllSchools = Map.fold (fun sum _ number -> sum+number) 0
-    Map.fold bringSchoolToNextGen Map.empty initialSchool |> countFishesInAllSchools
-
+        | 0 -> Map.change 6 (addToSchool value) newSchools |> Map.add 8 value
+        | 7 -> Map.change 6 (addToSchool value) newSchools
+        | e -> Map.add (e-1) value newSchools
+    let countFishesInAllSchools = Map.fold (fun sum _ number -> sum+number) 0L
+    let bringAllSchoolsToNextGen state _  = Map.fold bringSchoolToNextGen Map.empty state
+    [1..numberOfDays] |> List.fold bringAllSchoolsToNextGen initialSchool |> countFishesInAllSchools
+    
 [<Fact>]
 let ``Can count fish for 1 generation``() =
-    [2;3;2;0;1] |> countFishAfterDays 1 |> should equal 6
+    [2;3;2;0;1] |> countFishAfterDays 1 |> should equal 6L
     
+[<Fact>]
+let ``Can count fish for 2nd generation``() =
+    [2;3;2;0;1] |> countFishAfterDays 2 |> should equal 7L
     
+[<Fact>]
+let ``Can count fish for 80nd generation``() =
+    [3;4;3;1;2] |> countFishAfterDays 80 |> should equal 5934L
+
+
+[<Fact>]
+let ``Day 6 part 1``() =
+    System.IO.File.ReadAllText "day6Input.txt" |> FSharpx.String.splitChar [|','|]
+        |> Array.toList
+        |> List.map int
+        |> countFishAfterDays 80 |> should equal 393019L
+    
+[<Fact>]
+let ``Day 6 part 2``() =
+    System.IO.File.ReadAllText "day6Input.txt" |> FSharpx.String.splitChar [|','|]
+        |> Array.toList
+        |> List.map int
+        |> countFishAfterDays 256 |> should equal 1757714216975L
