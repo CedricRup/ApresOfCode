dc6197e16238dce6a42bd4837d473e9f88938bdc
Cédric Rup <cedric.rup@gmail.com>
2022-04-28 13:08:43 +0200

    Day 7 part 2

--- fsharp/Day7.fs
+++ fsharp/Day7.fs
@@ -1,26 +1,42 @@
 module Day7
 
-open System
 open Xunit
 open FsUnit.Xunit
-open FSharpx.String
 
 
-let calculateMinimumFuelForAlignement positions =
+let calculateMinimumFuelForAlignement fuelConsumptionStrategy positions =
     let minimum = List.min positions
     let maximum = List.max positions
-    let crabToPostion destination initial  = abs (initial - destination)
-    let allCrabs crabs position = List.sumBy (crabToPostion position) crabs
+    let allCrabs crabs position = List.sumBy (fuelConsumptionStrategy position) crabs
     [minimum..maximum] |> List.map  (allCrabs positions) |> List.min
-    
+
+let naiveConsumption destination initial  = abs (initial - destination)
+let naiveCrabAlignment = calculateMinimumFuelForAlignement naiveConsumption   
      
 [<Fact>]
 let ``From example``() =
-    [16;1;2;0;4;2;7;1;2;14] |> calculateMinimumFuelForAlignement |> should equal 37
+    [16;1;2;0;4;2;7;1;2;14] |> naiveCrabAlignment |> should equal 37
     
 [<Fact>]
 let ``Day 7 part 1``() =
     System.IO.File.ReadAllText "day7Input.txt" |> FSharpx.String.splitChar [|','|]
         |> Array.toList
         |> List.map int
-        |> calculateMinimumFuelForAlignement |> should equal 348664
+        |> naiveCrabAlignment |> should equal 348664
+
+let realConsumption destination initial =
+    let n = abs (destination - initial)
+    n * (n+1) / 2
+    
+let realCrabAlignment = calculateMinimumFuelForAlignement realConsumption
+
+[<Fact>]
+let ``Real consumption from example``() =
+    [16;1;2;0;4;2;7;1;2;14] |> realCrabAlignment |> should equal 168
+    
+[<Fact>]
+let ``Day 7 part 2``() =
+    System.IO.File.ReadAllText "day7Input.txt" |> FSharpx.String.splitChar [|','|]
+        |> Array.toList
+        |> List.map int
+        |> realCrabAlignment |> should equal 100220525
