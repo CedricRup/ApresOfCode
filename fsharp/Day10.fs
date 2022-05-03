b912606011e2ccb6feb27056cf5461640ce0dc9b
Cédric Rup <cedric.rup@gmail.com>
2022-05-03 15:53:54 +0200

    Day 10 part 2

--- fsharp/Day10.fs
+++ fsharp/Day10.fs
@@ -16,11 +16,12 @@ let sample = """[({(<(())[]>[[{[]{<()<>>
 <{([{{}}[<[[[<>{}]]]>[]]
 """
 
-let findFaulty string =
-    let areOpposite a b =
+let areOpposite a b =
         match (a,b) with
-        | ('<','>') | ('{','}') | ('[',']') | ('(',')') -> true
+        | '<','>' | '{','}' | '[',']' | '(',')' -> true
         | _ -> false
+
+let findFaulty string =
     let folder (_,stack) char =
         match char with
         | '(' | '[' | '{' | '<' -> (None,char::stack)
@@ -65,4 +66,51 @@ let ``Day 10 part Part 1`` () =
     |> Array.ofSeq
     |> calculateTotalSyntaxErrorScore
     |> should equal 411471
+
+let completeLineScore line =
+    let folder stack char =
+        match char with
+        | '(' | '[' | '{' | '<' -> char::stack
+        | c ->
+            match stack with
+            | head::tail when areOpposite head c  -> tail
+            | _ -> failwith "Should not happen"
+    let remainder = line |> FSharpx.String.toCharArray |> Array.fold folder List.Empty
+    let score = function
+        | '(' -> 1L
+        | '[' -> 2L
+        | '{' -> 3L
+        | '<' -> 4L
+        | _ -> failwith "Should not happen"
+    remainder |> List.fold (fun sum x -> (sum * 5L) + score x) 0L
+        
+    
+[<Fact>]
+let ``Complete line score`` () =
+    "[({(<(())[]>[[{[]{<()<>>"
+    |> completeLineScore
+    |> should equal 288957L
+
+let findMiddleScore strings =
+    let scores =
+        strings
+        |> Array.filter (findFaulty >> Option.isNone)
+        |> Array.map completeLineScore
+        |> Array.sort
+    scores[scores.Length / 2]
+    
+
+[<Fact>]
+let ``Part 2 sample`` () =
+    sample
+    |> FSharpx.String.splitString [|"\n"|] StringSplitOptions.RemoveEmptyEntries
+    |> findMiddleScore
+    |> should equal 288957L
+    
+[<Fact>]
+let ``Day 10 part Part 2`` () =
+    IO.File.ReadLines "day10Input.txt"
+    |> Array.ofSeq
+    |> findMiddleScore
+    |> should equal 3122628974L
 
