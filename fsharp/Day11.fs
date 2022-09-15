27ddafce6031b3fa3667f58a5e2fb3254ee06344
Cédric Rup <cedric.rup@gmail.com>
2022-09-15 13:37:33 +0200

    Refactorings

--- fsharp/Day11.fs
+++ fsharp/Day11.fs
@@ -33,7 +33,7 @@ let nextStep cavern =
         match toEnergize with
         | [] -> cavern
         | _ ->
-            let (cavern,toEnergize) = List.fold energizeFolder (cavern,[]) toEnergize
+            let cavern,toEnergize = List.fold energizeFolder (cavern,[]) toEnergize
             energize' cavern toEnergize
     let cavern = cavern |> Array2D.map (function Flashed -> Energy 0 | o -> o )        
     let toEnergize = Array2D.mapi (fun  y x  _ -> (x,y)) cavern |> Seq.cast<int * int> |> Seq.toList
@@ -41,54 +41,60 @@ let nextStep cavern =
     
     
 
-let step stepCount (cavern: Cavern) : int =
+let countIndividualFlashesDuringSteps stepCount (cavern: Cavern) : int =
     let folder (cavern : Cavern) _ = 
         cavern
         |> nextStep
+        
+    let numberOfFlashedOctopus cavern =
+        cavern
+        |> Seq.cast<Octopus>
+        |> Seq.filter (fun o -> o = Flashed)
+        |> Seq.length
         
    
     [1..stepCount]
     |> List.scan folder cavern
     |> List.skip 1
-    |> List.map (fun c -> c |> Seq.cast<Octopus> |> Seq.filter (fun o -> o = Flashed) |> Seq.length)
+    |> List.map numberOfFlashedOctopus
     |> Seq.sum
     
 [<Fact>]
 let ``Lonely octupus with low energy doesn't flash in one step`` () =
     let cavern = array2D [[0]] |> Array2D.map Energy
-    cavern |> step 1 |> should equal 0
+    cavern |> countIndividualFlashesDuringSteps 1 |> should equal 0
     
 [<Fact>]
 let ``Lonely octupus with low energy flashes with enough steps`` () =
     let cavern = array2D [[0]] |> Array2D.map Energy
-    cavern |> step 10 |> should equal 1
+    cavern |> countIndividualFlashesDuringSteps 10 |> should equal 1
     
 [<Fact>]
 let ``Lonely octupus with 5 energy flashes with 5 steps`` () =
     let cavern = array2D [[5]] |> Array2D.map Energy
-    cavern |> step 5 |> should equal 1
+    cavern |> countIndividualFlashesDuringSteps 5 |> should equal 1
     
 [<Fact>]
 let ``Lonely octupus with 5 energy flashes twice with 15 steps`` () =
     let cavern = array2D [[5]] |> Array2D.map Energy
-    cavern |> step 15 |> should equal 2
+    cavern |> countIndividualFlashesDuringSteps 15 |> should equal 2
     
     
 [<Fact>]
 let ``A couple of synchronized octupusses`` () =
     let cavern = array2D [[9;9]] |> Array2D.map Energy
-    cavern |> step 1 |> should equal 2
+    cavern |> countIndividualFlashesDuringSteps 1 |> should equal 2
 
 
 [<Fact>]
 let ``Lonely octopus reset energy after flash`` () =
     let cavern = array2D [[9]] |> Array2D.map Energy
-    cavern |> nextStep |> Seq.cast<Octopus> |> Seq.head |> should equal (Flashed)
+    cavern |> nextStep |> Seq.cast<Octopus> |> Seq.head |> should equal Flashed
     
 [<Fact>]
 let ``A couple of unsynchronized octupusses`` () =
     let cavern = array2D [[9;8]] |> Array2D.map Energy
-    cavern |> step 1 |> should equal 2
+    cavern |> countIndividualFlashesDuringSteps 1 |> should equal 2
  
 let example = @"5483143223
 2745854711
@@ -112,24 +118,26 @@ let parse (input : string) =
 [<Fact>]
 let ``Day 11 part 1 example `` () =
     let cavern = parse example
-    cavern |> step 100 |> should equal 1656
+    cavern |> countIndividualFlashesDuringSteps 100 |> should equal 1656
     
 [<Fact>]
 let ``Day 11 part 1  `` () =
     let cavern = IO.File.ReadAllText "day11Input.txt" |>parse 
-    cavern |> step 100 |> should equal 1721
+    cavern |> countIndividualFlashesDuringSteps 100 |> should equal 1721
     
 
 
 let firstSyncFlash (cavern: Cavern) : int =
     let folder (cavern : Cavern, _) stepNumber = 
         (cavern |> nextStep,stepNumber)
+    
+    let AtLeastOneOctupusIsStillEnergized cavern = cavern |> Seq.cast<Octopus> |> Seq.exists (fun o -> o <> Flashed)
         
    
     seq {1..Int32.MaxValue}
     |> Seq.scan folder (cavern,0)
     |> Seq.skip 1
-    |> Seq.skipWhile (fun (c,_) -> c |> Seq.cast<Octopus> |> Seq.exists (fun o -> o <> Flashed))
+    |> Seq.skipWhile (fst >> AtLeastOneOctupusIsStillEnergized)
     |> Seq.head
     |> snd
     
